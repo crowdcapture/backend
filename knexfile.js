ccd4fe27b82f1aef9f2d1765e643137382375c24
Pieter <gitkraken@pietervanmill.nl>
2020-01-18 19:31:48 +0100

    Production was missing in knexfile.

--- knexfile.js
+++ knexfile.js
@@ -30,6 +30,21 @@ module.exports = {
     seeds: {
       directory: __dirname + '/db/seeds/development'
     }
-  }
+  },
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: process.env.POSTGRES_DB,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      host: process.env.POSTGRES_HOST
+    },
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/development'
+    }
+  },
 }
 
