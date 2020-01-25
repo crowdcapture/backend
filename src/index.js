c9b708b3c9a7fa38921f717412852d545aa250e8
Pieter <gitkraken@pietervanmill.nl>
2020-01-25 18:36:46 +0100

    Added Sentry for better error tracking.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
-// BASE SETUP
-process.env.NODE_ENV = 'development';
-
-// call the packages we need
 const dotenv = require('dotenv');
-
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config();
-}
-
+const sentry = require('@sentry/node');
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-let port = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
 
+sentry.init({ dsn: process.env.SENTRY_KEY });
+
+const port = process.env.PORT || 5000;
 const routes = require('./routes');
 
 // configure bodyParser
@@ -33,9 +30,6 @@ app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  console.log(err);
-  
 
   // render the error page
   res.status(err.status || 500).send(err);
