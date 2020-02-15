003b8bcef609ad99c80fb7306f679a0b4b3528d0
Pieter <gitkraken@pietervanmill.nl>
2020-02-15 16:45:44 +0100

    Having no projects is not an error.

--- src/project/projects.my.get.js
+++ src/project/projects.my.get.js
@@ -4,12 +4,10 @@ async function getMyProjects(req, res, next) {
     try {
         const projects = await queries.getMyProjects(req.user.id);
 
-        if (projects.length === 0) {
-            throw({ success: false, status: 400, message: 'You haven\'t made any projects yet.' });
-        }
-
         res.status(200);
-        res.send({success: true, projects: projects});
+        res.send({
+            success: true, projects: projects || []
+        });
     } catch (error) {
         next(error);
     }
